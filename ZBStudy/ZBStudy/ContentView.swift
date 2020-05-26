94a801f5b9272b2d177695031be28184f999f42e
zhaobing <zhaobing@chuanghehui.cn>
2020-05-26 15:45:15 +0800

    init

---
+++ ZBStudy/ZBStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZBStudy
+//
+//  Created by 赵兵 on 2020/5/26.
+//  Copyright © 2020 赵兵. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZBStudy/ZBStudyTests/ZBStudyTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZBStudyTests.swift
+//  ZBStudyTests
+//
+//  Created by 赵兵 on 2020/5/26.
+//  Copyright © 2020 赵兵. All rights reserved.
+//
+
+import XCTest
+@testable import ZBStudy
+
+class ZBStudyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
